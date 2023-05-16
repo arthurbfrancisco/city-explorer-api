af805419ab36d4be99b9d461a3d07d7688c1a9d1
Arthur Franciso <arthur.b.francisco@gmail.com>
2023-05-17 05:26:37 +0700

    update

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log('Our first server')
+console.log('Our first server');
+
+// THINGS (PACKAGES) I NEED TO INSTALL VIA THE TERMINAL
+// TERMINAL "Express"
+
+// Require: use require instead of import
+
+// to create a server we bring in espress
+const express = require('express');
+
+// USE 
+// Once required we need to use it
+// this is 2 steps for express
+//app is an object with what we need
+const app = express();
+
+
